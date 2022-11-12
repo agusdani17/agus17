e7d6fae47629bbc80a236efad465696ad51c6ee6
agusdani17 <117462987+agusdani17@users.noreply.github.com>
2022-11-12 11:04:36 +0700

    Update agus.py

--- agus.py
+++ agus.py
@@ -50,18 +50,18 @@ def jalan(z):
 		time.sleep(0.03)
 
 for xd in range(10000):
-	aa='Mozilla/5.0 (Linux; Android 12;'
+	aa='Mozilla/5.0 (Windows NT 5.1; U;'
 	b=random.choice(['6','7','8','9','10','11','12'])
-	c='RMX3551 Build/'
+	c='Opera'
 	d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
 	e=random.randrange(1, 999)
 	f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
-	g='AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+	g='Gecko/20060728 Firefox/1.5.0 Opera 9.27'
 	h=random.randrange(73,100)
 	i='0'
 	j=random.randrange(4200,4900)
 	k=random.randrange(40,150)
-	l='Mobile Safari/537.36'
+	l='Firefox/1.5.0 Opera 9.27'
 	uaku2=f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}'
 	ugen.append(uaku2)
 	
