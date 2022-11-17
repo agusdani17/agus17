f07f37d28120cd0d31f1c7fd073d4adcf23a9731
agusdani17 <117462987+agusdani17@users.noreply.github.com>
2022-11-17 16:33:53 +0700

    Update agus.py

--- agus.py
+++ agus.py
@@ -41,7 +41,7 @@ for xd in range(10000):
 	uaku=(f'{a}{b}.{c} {d}{e}{f}{g}.{h}.{i}.{j} {k}')
 	ugen2.append(uaku)
 
-	aa='ozilla/5.0 (Linux; Android'
+	aa='Mozilla/5.0 (Linux; Android'
 	b=random.choice(['6','7','8','9','10','11','12'])
 	c='LG-'
 	d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
