0581555c6cd9df7e03a37d78480834febac98ad1
agusdani17 <117462987+agusdani17@users.noreply.github.com>
2022-11-29 11:31:54 +0800

    Update agus.py

--- agus.py
+++ agus.py
@@ -44,17 +44,21 @@ except Exception as e:
 	print('[[\x1b[1;92m•\x1b[1;97m] [\x1b[1;96mAlvino_adijaya_xy')
 prox=open('.prox.txt','r').read().splitlines()
 for xd in range(10000):
-	a='"Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv)'
+	a='Mozilla/5.0 (iPhone; CPU iPhone OS 8_1 like Mac OS X) Gecko/20100101 Firefox/31.0'
+	a='Mozilla/5.0 (X11; U; Linux amd64;'
 	b=random.randrange(1, 9)
 	c=random.randrange(1, 9)
-	d='Mi 9T Pro'
+	d='iPhone OS'
+	d='Linux'
 	e=random.randrange(100, 9999)
-	f='AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+	f='/110.021.0028; Profile/MIDP-2.1 Configuration/CLDC-1.1 ) CPU iPhone OS 8_1 like Mac OS X) Gecko/20100101 Firefox/31.0'
+	f='en-US; rv:5.0) Gecko/20110619 Firefox/5.0'
 	g=random.randrange(1, 9)
 	h=random.randrange(1, 4)
 	i=random.randrange(1, 4)
 	j=random.randrange(1, 4)
-	k='Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]'
+	k='Firefox/31.0'
+	k='Firefox/5.0'
 	uaku=(f'{a}{b}.{c} {d}{e}{f}{g}.{h}.{i}.{j} {k}')
 	ugen2.append(uaku)
 
